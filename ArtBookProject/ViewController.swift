46b7a3b812da0417770d9612ecf478f8baa9967a
Kubraakk <kbr3460@icloud.com>
2024-03-13 00:07:44 +0300

    Initial Commit

---
+++ ArtBookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBookProject
+//
+//  Created by Kübra Akpunar (Teknoloji Mimarisi Bölümü) on 13.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
